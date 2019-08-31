49fffe9a1b02aff1b1285202f4110f722e62c969
Bruno Adamo <brunoaraujoadamo@gmail.com>
2019-08-31 17:21:29 -0300

    AppointmentController: List all appointments

--- src/routes.js
+++ src/routes.js
@@ -23,6 +23,8 @@ routes.put('/users', UserController.update);
 routes.get('/providers', ProviderController.index);
 
 routes.post('/appointments', AppointmentController.store);
+routes.get('/appointments', AppointmentController.index);
+
 routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
