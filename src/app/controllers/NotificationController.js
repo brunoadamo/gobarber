5a396e1e2015cfbc66837224ae6c82a5d6c798f1
Bruno Adamo <brunoaraujoadamo@gmail.com>
2019-09-02 20:28:38 -0300

    Notification Controller

---
+++ src/app/controllers/NotificationController.js
@@ -0,0 +1,9 @@
+import Notification from '../schemas/Notification';
+
+class NotificationControler {
+  async index(req, res) {
+    return res.json();
+  }
+}
+
+export default new NotificationControler();
