72e757013ac8b2af9159f91e834fea5b3e86cc86
Bruno Adamo <brunoaraujoadamo@gmail.com>
2019-10-01 20:58:25 -0300

    feature: nodemail and config

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '467daed0f4a909',
+    pass: '8f6898251a5c9b',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
