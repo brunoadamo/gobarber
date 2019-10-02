c2c2028a2c3a6da4dd4ff4e1395e27fe692efd9c
Bruno Adamo <brunoaraujoadamo@gmail.com>
2019-10-01 21:40:22 -0300

    feature: handlebars - templates for email

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -50,7 +50,7 @@ class AppointmentController {
      */
 
     const idProvider = await User.findOne({
-      where: { id: provider_id, provider: true },
+      where: { id: req.userId, provider: true },
     });
 
     if (!idProvider) {
@@ -126,6 +126,11 @@ class AppointmentController {
           as: 'provider',
           attributes: ['name', 'email'],
         },
+        {
+          model: User,
+          as: 'user',
+          attributes: ['name'],
+        },
       ],
     });
 
@@ -150,7 +155,14 @@ class AppointmentController {
     await Mail.sendMail({
       to: `${appointment.provider.name} <${appointment.provider.email}>`,
       subject: 'Agendamento cancelado',
-      text: 'Você tem um novo cancelamento',
+      template: 'cancellation',
+      context: {
+        provider: appointment.provider.name,
+        user: appointment.user.name,
+        date: format(appointment.date, "'dia' dd 'de' MMMM', às' H:mm'h'", {
+          locale: pt,
+        }),
+      },
     });
 
     return res.json(appointment);
