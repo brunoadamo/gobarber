8fff849d0fa320001d62f58c0731d3de1e85bd97
Bruno Adamo <brunoaraujoadamo@gmail.com>
2019-10-02 21:03:20 -0300

    available appointments hours

--- src/app/controllers/AvailableController.js
+++ src/app/controllers/AvailableController.js
@@ -1,4 +1,12 @@
-import { startOfDay, endOfDay } from 'date-fns';
+import {
+  startOfDay,
+  endOfDay,
+  setHours,
+  setMinutes,
+  setSeconds,
+  format,
+  isAfter,
+} from 'date-fns';
 import { Op } from 'sequelize';
 import Appointment from '../models/Appointment';
 
@@ -22,7 +30,41 @@ class AvailableController {
       },
     });
 
-    return res.json(appointments);
+    // 2018-06-23 23:33:22
+
+    const schedule = [
+      '08:00', // 2018-06-23 08:00:00
+      '09:00',
+      '10:00',
+      '11:00',
+      '12:00',
+      '13:00',
+      '14:00',
+      '15:00',
+      '16:00',
+      '17:00',
+      '18:00',
+      '19:00',
+      '23:00',
+    ];
+
+    const available = schedule.map(time => {
+      const [hour, minute] = time.split(':');
+      const value = setSeconds(
+        setMinutes(setHours(searchDate, hour), minute),
+        0
+      );
+
+      return {
+        time,
+        value: format(value, "yyyy-MM-dd'T'HH:mm:ssxxx"),
+        available:
+          isAfter(value, new Date()) &&
+          !appointments.find(a => format(a.date, 'HH:mm') === time),
+      };
+    });
+
+    return res.json(available);
   }
 }
 
